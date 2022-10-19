abd4617a715179486264db02fa476f103d4e5d8b
Vladlen <ilusenkovladlen@gmail.com>
2022-10-19 22:48:53 +0300

    feat: added @vercel/node

--- api/hello.ts
+++ api/hello.ts
@@ -1,3 +1,5 @@
-export default (req: any, res: any): void => {
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+
+export default function(req: VercelRequest, res: VercelResponse) {
   res.json({ message: 'Your request was successfully handled' });
 };
