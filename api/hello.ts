7319a40effb0e8bc2923999ffdadb56341d6a265
Vladlen <ilusenkovladlen@gmail.com>
2022-10-19 22:55:10 +0300

    fix: prettier is working on all types of files now

--- api/hello.ts
+++ api/hello.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
-export default function(req: VercelRequest, res: VercelResponse) {
+export default function (req: VercelRequest, res: VercelResponse) {
   res.json({ message: 'Your request was successfully handled' });
-};
+}
