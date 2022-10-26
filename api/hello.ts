795108b3d1486ab5b0acd021535be63829d3db4b
Vladlen Ilyushchenko <71406730+Ekedani@users.noreply.github.com>
2022-10-27 01:12:28 +0300

    Laboratory work №1 completed (#1)
    * feat: added basic dev dependencies
    
    * feat: configured eslint
    
    * feat:configured prettier
    
    * feat: added Lint github action
    
    * fix: fixed script name in Lint action
    
    * feat: added typescript
    
    * feat: added .editorconfig
    
    * fix: fixed tabs in .editorconfig
    
    * feat: added ts-node
    
    * fix: changed typescript compiller options
    
    * feat: added @vercel/node
    
    * fix: prettier is working on all types of files now
    
    * fix: applied .editorconfig
    
    * feat: deleted rule from .editorconfig
    
    * fix: prettier after sonar
    
    * fix: an attempt to fix actions.yml
    
    * fix: removed ts-node
    
    * fix: returned insert_final_newline rule

---
+++ api/hello.ts
@@ -0,0 +1,5 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+
+export default function (req: VercelRequest, res: VercelResponse) {
+  res.json({ message: 'Your request was successfully handled' });
+}
